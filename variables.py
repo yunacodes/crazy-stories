c9c73f50012896638406f066c5f6248b6811b37b
Evelyn Shepherd <unicornkitty@Macintosh-2.matthome.local>
2019-01-06 09:53:12 -0500

    Chapter 2 from coding book exercises

---
+++ variables.py
@@ -0,0 +1,32 @@
+#!/usr/local/bin/python3
+print("NUMBERS")
+age = 8
+print (age)
+x = 6
+y = x * 7
+x = 10
+print (y)
+x = 10
+y = x * 7
+print (y)
+print("STRINGS")
+name = 'ally alien'
+print (name)
+name = 'ally alien'
+greeting = 'welcome to earth,'
+message = greeting + name
+print (message)
+rockets_players_1 = 'rory'
+rockets_players_2 = 'rav'
+rockets_players_3 = 'rachel'
+planets_players_1 = 'peter'
+planets_players_2 = 'pablo'
+planets_players_3 = 'polly'
+print("LISTS")
+rockets_players = ['rory','rav','rachel','renata','ryan','ruby']
+
+planets_players = ['peter','pablo','polly','penny','paula','patrick']
+
+print(rockets_players[0])
+
+planets_players[5]
